54e828d575d317719f7b6b259c2af6d64cb9d4f2
Nicholas Moen <arcanemachine@tutanota.com>
2022-11-25 20:48:33 -0700

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../theme/variables.css';
+	import { setupIonicSvelte } from 'ionic-svelte';
+
+	setupIonicSvelte();
+</script>
+
+<ion-app>
+	<ion-page>
+		Hi there <ion-button>test</ion-button>
+	</ion-page>
+</ion-app>
